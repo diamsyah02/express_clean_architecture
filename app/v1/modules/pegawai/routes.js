c1332311098556b8fe1c88b02ab2329d62ba1714
Diamsyah M Dida <didadiamsyah02@gmail.com>
2022-03-16 10:00:21 +0700

    update route pegawai

--- app/v1/modules/pegawai/routes.js
+++ app/v1/modules/pegawai/routes.js
@@ -3,12 +3,12 @@ const AuthMiddleware = require('../../../../helpers/middleware/AuthMiddleware')
 const controller = require('./controller')
 
 async function PegawaiRoute(app, prefix) {
-  await app.route(`${prefix}/pegawai`).get(AuthMiddleware, controller.fetchAll)
+  await app.use(`${prefix}/pegawai`, AuthMiddleware)
+  await app.route(`${prefix}/pegawai`).get(controller.fetchAll)
   await app.route(`${prefix}/pegawai/:id`).get(AuthMiddleware, controller.fetchDetail)
-  await app.route(`${prefix}/pegawai`).post(AuthMiddleware, controller.store)
-  await app.route(`${prefix}/pegawai/:id`).post(AuthMiddleware, controller.update)
-  await app.route(`${prefix}/pegawai/:id/delete`).get(AuthMiddleware, controller.remove)
-
+  await app.route(`${prefix}/pegawai`).post(controller.store)
+  await app.route(`${prefix}/pegawai/:id`).post(controller.update)
+  await app.route(`${prefix}/pegawai/:id/delete`).get(controller.remove)
 }
 
 module.exports = PegawaiRoute
