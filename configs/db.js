fc7c093dc1aa3e8ddadc556f77777bed962c2ae7
Diamsyah M Dida <didadiamsyah02@gmail.com>
2022-03-17 14:51:13 +0700

    update db.js

--- configs/db.js
+++ configs/db.js
@@ -6,9 +6,9 @@ const option = {
   client: process.env.DB_CLIENT,
   connection: {
     host : process.env.DB_HOST,
-    port : process.env.DB_PORT,
+    port : process.env.PORT,
     user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
+    password : process.env.DB_PASS,
     database : process.env.DB_NAME
   },
   pool: {
